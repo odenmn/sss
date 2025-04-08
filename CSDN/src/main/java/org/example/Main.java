cb4d3660f225f78a6f252d000c8d11ad0c9ae3c7
odenmn <3461727951@qq.com>
2025-04-08 15:38:46 +0800

    测试程序

--- CSDN/src/main/java/org/example/Main.java
+++ CSDN/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import com.xjx.example.view.StartView;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new StartView().showMenu();
     }
 }
